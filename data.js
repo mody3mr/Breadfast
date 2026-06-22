e33e366853716823522b102dfc2ec0e535874e59
mody3mr <99924090+mody3mr@users.noreply.github.com>
2026-06-22 04:35:31 +0300

    Add branches data structure with locations

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
+const branches = [
 
+{
+id: 1,
+name: "Almaza",
+nameAr: "الماظة",
+map: "https://goo.gl/maps/S1VEK49xuYqERfTS8",
+manager: "",
+phone: "",
+whatsapp: "",
+hours: ""
+},
+
+{
+id: 2,
+name: "Madinaty",
+nameAr: "مدينتي",
+map: "https://goo.gl/maps/oizswESeDQn3qPE9A",
+manager: "",
+phone: "",
+whatsapp: "",
+hours: ""
+}
+
+];
